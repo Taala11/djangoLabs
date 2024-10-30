27513830cf1df933d9bf20322e0d5d391947ed2e
Taala11 <411201913@qu.edu.sa>
2024-10-30 16:32:40 +0300

    First commit

---
+++ apps/bookmodule/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+#from django.http import HttpResponse
+def index(request):
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
